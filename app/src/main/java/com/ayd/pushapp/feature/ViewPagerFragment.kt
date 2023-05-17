eb7cbb6951f3f3fa3e83b89819954f6b7727663c
Ali Atilla Aydemir <atilla734@gmail.com>
2023-05-17 22:48:18 +0300

    navigation adjusted.

--- app/src/main/java/com/ayd/pushapp/feature/ViewPagerFragment.kt
+++ app/src/main/java/com/ayd/pushapp/feature/ViewPagerFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
 import com.ayd.pushapp.adapter.ViewPagerAdapter
 import com.ayd.pushapp.databinding.FragmentViewPagerBinding
 import com.ayd.pushapp.feature.pagerfragments.*
